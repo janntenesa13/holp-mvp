86fb5742d689f3b99f8befc66dcb1e1003136700
janntenesa13 <janialba300@gmail.com>
2026-09-15 12:22:58 +0200

    Activate HOLP v7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="holp-v6";
-const ASSETS=["./","./index.html","./styles.css?v=6","./app.js?v=6","./manifest.webmanifest?v=6","./icon.svg"];
+const CACHE="holp-v7";
+const ASSETS=["./","./index.html","./styles.css?v=7","./app.js?v=7","./manifest.webmanifest?v=7","./icon.svg"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
