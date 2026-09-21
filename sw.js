a3504aa9949790981df247408d5c633220efb874
janntenesa13 <janialba300@gmail.com>
2026-09-21 19:56:15 +0200

    Refresh HOLP v12 cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="holp-v11";
-const ASSETS=["./","./index.html","./styles.css?v=11","./app.js?v=11","./manifest.webmanifest?v=11","./icon.svg"];
+const CACHE="holp-v12";
+const ASSETS=["./","./index.html","./styles.css?v=12","./app.js?v=12","./manifest.webmanifest?v=12","./icon.svg"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
